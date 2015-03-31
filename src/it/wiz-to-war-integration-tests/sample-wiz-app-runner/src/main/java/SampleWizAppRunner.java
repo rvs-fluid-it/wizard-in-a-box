f919ebc30bfbc6256e41cbddcc0f5a78286db730
rokoe <koen.roevens@gmail.com>
2015-03-31 19:43:21 +0200

    Renamed folder to wizard-in-a-box...

--- src/it/wiz-to-war-integration-tests/sample-wiz-app-runner/src/main/java/SampleWizAppRunner.java
+++
@@ -1,8 +0,0 @@
-import be.fluid_it.tools.dw.wiz2war.ci.app.HelloWorldApplication;
-import be.fluid_it.tools.dw.wiz2war.it.support.WizRunner;
-
-public class SampleWizAppRunner {
-    public static void main(String[] args) throws Exception {
-        new WizRunner(new HelloWorldApplication(), "./target/classes/hello-world-as-dw-jar.yml").run();
-    }
-}
--- src/it/wiz-to-war-integration-tests/test-support/src/main/java/be/fluid_it/tools/dropwizard/box/it/support/ContextUrlProvider.java
+++
@@ -1,5 +0,0 @@
-package be.fluid_it.tools.dropwizard.box.it.support;
-
-public interface ContextUrlProvider {
-    String contextUrl();
-}
